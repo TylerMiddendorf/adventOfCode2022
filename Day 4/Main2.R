44213aba0f7ce2464ffe659a066055e1d31aae88
Tyler Middendorf <60022505+TylerMiddendorf@users.noreply.github.com>
2022-12-06 17:43:13 -0600

    Finished Day 4 pt 2

--- Day 4/Main2.R
+++ Day 4/Main2.R
@@ -8,7 +8,7 @@ for(row in 1:nrow(combined_data)){
       num2 <- as.integer(combined_data[row,2])
       num3 <- as.integer(combined_data[row,3])
       num4 <- as.integer(combined_data[row,4])
-      if((num1 <= num3 & num2 >= num4) | (num1 >= num3 & num2 <= num4))
+      if(num2 >= num3 & num1 <= num4)
             counter <- counter + 1;
 }
 print(counter)
